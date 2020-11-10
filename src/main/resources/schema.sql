52733fd6ba2a060c3770b59140f692bd34d963ef
ClearJulyComes <viktordomogashev@gmail.com>
2020-11-11 01:09:25 +0300

    Added index and foreigh key

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS Brand (
+    id          INTEGER                         COMMENT 'Unique id'      PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(50) NOT NULL    UNIQUE  COMMENT 'Brand name'
+);
+
+CREATE TABLE IF NOT EXISTS Product (
+    id          INTEGER                 COMMENT 'Unique id'      PRIMARY KEY AUTO_INCREMENT,
+    brand_id    INTEGER     NOT NULL    COMMENT 'Brand id',
+    name        VARCHAR(50) NOT NULL    COMMENT 'Product name',
+    price       INTEGER     NOT NULL    COMMENT 'Product price',
+    quantity    INTEGER     NOT NULL    COMMENT 'Product quantity',
+    CONSTRAINT UC_Product UNIQUE (brand_id, name),
+    FOREIGN KEY (brand_id) REFERENCES Brand (id)
+);
+
+CREATE INDEX IX_Product_Name ON Product (name);
